b5b37d89329e577ff9f6d11f0b86be01edf554b2
jisegura <segurajuanignacio@outlook.com>
2017-02-07 19:23:05 -0300

    fix nav

--- app/js/nav.js
+++ app/js/nav.js
@@ -3,9 +3,17 @@
 $(document).ready(function() {
 	$('.navbar-toggle').click(function() {
 		if (!$('.navbar-collapse').hasClass('collapsing')) {
-			$('nav.navbar').toggleClass('open').one('transitionend', function(){
-				$('.navbar-toggle').toggleClass('closeNav');
-			});
+			if ($('nav.navbar').hasClass('open')) {
+				$('nav.navbar').removeClass('openAnimation').one('transitionend', function(){
+					$('.navbar-toggle').toggleClass('closeNav');
+					$('nav.navbar').removeClass('open');
+				});
+			} else {
+				$('nav.navbar').addClass('open');
+				$('nav.navbar').addClass('openAnimation').one('transitionend', function(){
+					$('.navbar-toggle').toggleClass('closeNav');
+				});
+			}
 		}
 	});
 
