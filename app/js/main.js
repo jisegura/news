acf0eca86629e21fa62b44115a62fa6a58db131c
jisegura <segurajuanignacio@outlook.com>
2017-02-10 16:32:10 -0300

    add 50% animations

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const endAnimation = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+function setLoaderDisplay(display) {
+	$('#loader-wrapper').css({'display': display});
+}
+
+function animationLoader() {
+	$('#loader-wrapper').addClass('loaded');
+	$('.loader-section').one('transitionend', function() {
+		setLoaderDisplay('none');
+		$('#loader-wrapper').removeClass('loaded');
+	});
+}
+
+function animationNavbar() {
+	$('nav.navbar').addClass('fadeInDown').one(endAnimation, function() {
+		$(this).removeClass('fadeInDown');
+	});
+}
+
+function animationLogo() {
+	$('#logo').addClass('zoomInDown').one(endAnimation, function() {
+		$(this).removeClass('zoomInDown');
+	});
+}
