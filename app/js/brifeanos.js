7a55dfb1b17cf9b8b726adcaf72a1fbd1c324ad4
tgispert <tomasgispert@gmail.com>
2017-10-12 20:44:19 -0300

    mail andando

--- app/js/brifeanos.js
+++ app/js/brifeanos.js
@@ -1,44 +1,49 @@
-$("#boton-comentario").click(function(event) {
+$("#brifeanos-send").click(function(event) {
     event.preventDefault();
     var form = event.target;
     var nombre = "";
     var mail = "";
-    var asunto = "";
+    var empresa = "";
+    var telefono = "";
     var comentario = "";
-    nombre = $("#contacto-nombre").val();
-    mail = $("#contacto-email").val();
-    asunto = $("#contacto-asunto").val();
-    comentario = $("#contacto-comentario").val();
-    if(nombre!=""&&mail!=""&&asunto!=""&&comentario!=""){
+    nombre = $("#brifeanos-nombre").val();
+    mail = $("#brifeanos-email").val();
+    empresa = $("#brifeanos-empresa").val();
+    telefono = $("#brifeanos-telefono").val();
+    comentario = $("#brifeanos-comentario").val();
+    if(nombre!=""&&mail!=""&&empresa!=""&&telefono!=""&&comentario!=""){
       var data = {
         "nombre": nombre,
-        "email": mail,
-        "asunto": asunto,
+        "empresa": empresa,
+        "mail": mail,
+        "telefono": telefono,
         "comentario": comentario
       };
-      $('#boton-comentario').html('Enviando... <i class="fa fa-paper-plane" aria-hidden="true"></i>');
+      $('#brifeanos-send').html('Enviando... <i class="fa fa-paper-plane" aria-hidden="true"></i>');
       $.ajax({
           type: "POST",
           url: "vendor/email/contacto.php",
           data: data,
           success: function(){
-            $("#contacto-error-message").addClass("hidden");
+            /* $("#contacto-error-message").addClass("hidden");
             $("#contacto-info-message").addClass("hidden");
-            $("#contacto-success-message").removeClass("hidden");
-            $('#boton-comentario').html('Enviar <i class="fa fa-paper-plane" aria-hidden="true"></i>');
+            $("#contacto-success-message").removeClass("hidden"); */
+            console.log("enviado");
+            $('#brifeanos-send').html('Enviado <i class="fa fa-paper-plane" aria-hidden="true"></i>');
           },
           error:function () {
-            $("#contacto-success-message").addClass("hidden");
+            /* $("#contacto-success-message").addClass("hidden");
             $("#contacto-info-message").addClass("hidden");
-            $("#contacto-error-message").removeClass("hidden");
-            $('#boton-comentario').html('Enviar <i class="fa fa-paper-plane" aria-hidden="true"></i>');
+            $("#contacto-error-message").removeClass("hidden"); */
+            console.log("error");
+            $('#brifeanos-send').html('Error <i class="fa fa-paper-plane" aria-hidden="true"></i>');
           }
       });
     }
     else {
-      $("#contacto-error-message").addClass("hidden");
+      /* $("#contacto-error-message").addClass("hidden");
       $("#contacto-success-message").addClass("hidden");
-      $("#contacto-info-message").removeClass("hidden");
+      $("#contacto-info-message").removeClass("hidden"); */
     }
 
 });
